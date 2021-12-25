589739a41e1f88de2f621092dc5ab7c23850201c
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-25 22:47:11 +0600

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,18 @@ let searchUser = document.querySelector("#searchUser");
 
 searchBtn.addEventListener("click", (e) => {
   let userText = searchUser.value;
+  if (userText != " ") {
+    //Fetch Api
+    fetch(`https://api.github.com/users/${userText}`)
+      .then((result) => result.json())
+      .then((data) => {
+        if (data.message == "Not Found") {
+          //Show Alert
+        } else {
+          //Show Profile
+        }
+      });
+  } else {
+    //clear profile
+  }
 });
