41f8b6cb61ec280b71b4e3aa6353e039c8ac1ed9
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-26 02:00:47 +0600

    update

--- src/js/ui.js
+++ src/js/ui.js
@@ -5,24 +5,33 @@ class UI {
 
   showProfile(user) {
     this.clearAlert();
-    this.profile.innerHTML = `<div class="card card-body mb-3">
+    this.profile.innerHTML = ` <div class="card card-body mb-3">
     <div class="row">
       <div class="col-lg-3">
-        <img src="${user.avatar_url}" class="img-fluid rounded" alt="">
-        <a href="${user.html_url}" class="btn btn-primary btn-block mt-4">View Profile</a>
+        <img
+          src="${user.avatar_url}"
+          class="img-fluid rounded" alt="">
+        <div class="text-center">
+          <h1>${user.name}</h1>
+          <p>${user.login}</p>
+          <p>${user.bio}</p>
+        </div>
+        <a href="${user.html_url}" class="btn btn-primary btn-block mt-3 mb-3 w-100">View Profile</a>
+        <span class="badge bg-success">Followers: ${user.followers}</span>
+        <span class="badge bg-warning">Following: ${user.following}</span>
+        <ul class="text-left mt-4 mb-2 p-0">
+          <li class="list-group-item">Company: ${user.company}</li>
+          <li class="list-group-item">Website: <a style="text-decoration: none;" href="http://${user.blog}">${user.blog}</a></li>
+          <li class="list-group-item">Address: ${user.location}</li>
+          <li class="list-group-item">Member Since: ${user.created_at}</li>
+        </ul>
+
       </div>
       <div class="col-lg-9">
         <span class="badge bg-primary">Public Repos: ${user.public_repos}</span>
         <span class="badge bg-info">Public Gists: ${user.public_gists}</span>
-        <span class="badge bg-success">Followers: ${user.followers}</span>
-        <span class="badge bg-warning">Following: ${user.following}</span>
         <br><br>
-        <ul>
-          <li class="list-group-item">Company: ${user.company}</li>
-          <li class="list-group-item">Website: ${user.blog}</li>
-          <li class="list-group-item">Address: ${user.location}</li>
-          <li class="list-group-item">Member Since: ${user.created_at}</li>
-        </ul>
+
       </div>
     </div>
   </div>`;
