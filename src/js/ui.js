95af17728748f643031909315d09bf39614df279
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-26 03:24:05 +0600

    sticky

--- src/js/ui.js
+++ src/js/ui.js
@@ -6,9 +6,11 @@ class UI {
 
   showProfile(user) {
     this.clearAlert();
-    this.profile.innerHTML = ` <div class="card card-body mb-3">
+    this.profile.innerHTML = ` 
+    <div class="card card-body mb-3">
     <div class="row">
       <div class="col-lg-3">
+        <div class="sticky-top">
         <img
           src="${user.avatar_url}"
           class="img-fluid rounded" alt="">
@@ -27,8 +29,8 @@ class UI {
           <li class="list-group-item">Address: ${user.location}</li>
           <li class="list-group-item">Member Since: ${user.created_at}</li>
         </ul>
-
       </div>
+        </div>
       <div class="col-lg-9">
         <span class="badge bg-primary">Public Repos: ${user.public_repos}</span>
         <span class="badge bg-info">Public Gists: ${user.public_gists}</span>
