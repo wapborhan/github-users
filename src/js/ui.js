a1b6811d9c3621330e2543e0e9d14ab31ef521f8
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-25 23:20:20 +0600

    bug fix

--- src/js/ui.js
+++ src/js/ui.js
@@ -4,6 +4,26 @@ class UI {
   }
 
   showProfile(user) {
-    this.profile.innerHTML = "";
+    this.profile.innerHTML = `<div class="card card-body mb-3">
+    <div class="row">
+      <div class="col-lg-3">
+        <img src="${user.avatar_url}" class="img-fluid rounded" alt="">
+        <a href="${user.html_url}" class="btn btn-primary btn-block mt-4">View Profile</a>
+      </div>
+      <div class="col-lg-9">
+        <span class="badge bg-primary">Public Repos: ${user.public_repos}</span>
+        <span class="badge bg-info">Public Gists: ${user.public_gists}</span>
+        <span class="badge bg-success">Followers: ${user.followers}</span>
+        <span class="badge bg-warning">Following: ${user.following}</span>
+        <br><br>
+        <ul>
+          <li class="list-group-item">Company: ${user.company}</li>
+          <li class="list-group-item">Website: ${user.blog}</li>
+          <li class="list-group-item">Address: ${user.location}</li>
+          <li class="list-group-item">Member Since: ${user.created_at}</li>
+        </ul>
+      </div>
+    </div>
+  </div>`;
   }
 }
