74db08a7c1315d9848166fea36d95076ab9d1b6f
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-12-25 22:37:53 +0600

    Update main.js

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+let searchBtn = document.querySelector("#searchBtn");
+let searchUser = document.querySelector("#searchUser");
+
+searchBtn.addEventListener("click", (e) => {
+  let userText = searchUser.value;
+});
