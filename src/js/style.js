e663f290a7f69daf73d3b5c1a8c52ab4f16155ac
Borhan Uddin <borhaninfos@gmail.com>
2022-01-12 18:03:28 +0600

    class fix

--- src/js/style.js
+++ src/js/style.js
@@ -1,8 +1,31 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  var x = document.getElementById("mysr_topnav");
+  if (x.className === "sr_topnav") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "sr_topnav";
   }
+}
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
